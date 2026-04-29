c31a05d41b18dabbd25355cee6514c04a579b766
AgustinJG8 <agustin.jg02@gmail.com>
2026-04-29 14:35:06 -0600

    Fix: evitar crasheo de supabase en el build de cloudflare

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import Stripe from "stripe";
 import { supabase } from "../../lib/supabase";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || 'sk_dummy_key');
 
 export async function POST(req: Request) {
     try {
