be533e1702494b83ac63b475ec4f41a6eacde21a
AgustinJG8 <agustin.jg02@gmail.com>
2026-05-05 09:40:55 -0600

    Fix: actualizar URL y Anon Key de Supabase

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://dummy.supabase.co'
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'dummy'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://azaexskhbjkrbdsvjewr.supabase.co'
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF6YWV4c2toYmprcmJkc3ZqZXdyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU2NjE3MDMsImV4cCI6MjA5MTIzNzcwM30.BlGauqNaMAtDpbzWHRC85WoSg7fyM5aC24rPS55FVpw'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
